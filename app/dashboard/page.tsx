517e6febfe8974b61540db83881e33e4d0aee1dc
Tony Acaldo <tacaldo@gmail.com>
2026-01-12 10:41:54 -0600

    Latest cleanup.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,9 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchRevenue, 
+         fetchLatestInvoices, 
+         fetchCardData } from '@/app/lib/data';
  
 
 export default async function Page() {
