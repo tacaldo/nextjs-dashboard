ce8cf7abc5664e058b3114d0c054059906d209e6
Tony Acaldo <tacaldo@gmail.com>
2026-01-15 17:10:45 -0600

    CH 10 work

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -10,8 +11,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {   
+    
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -20,7 +24,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     replace(`${pathname}?${params.toString()}`);
 
 
-  }
+  }  , 300);
 
 
   return (
