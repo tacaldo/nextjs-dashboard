dbb40479f86cb7a554ddc7ffdf7cab6cbf610012
Tony Acaldo <tacaldo@gmail.com>
2026-01-21 09:32:31 -0600

    Latest cleanup..

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -50,7 +50,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-   
+
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
@@ -69,24 +69,34 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
     await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
   } catch (error) {
-    // We'll also log the error to the console for now
-    console.error(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
  
@@ -101,3 +111,4 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
+
